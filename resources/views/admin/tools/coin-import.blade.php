16285f3f5f3e70ee35b32c0d183152923d37cc9d
Yabo <386139859@qq.com>
2020-04-10 13:28:22 +0800

    :art: update modal style

--- resources/views/admin/tools/coin-import.blade.php
+++ resources/views/admin/tools/coin-import.blade.php
@@ -20,20 +20,38 @@
         opacity: 0;
     }
 </style>
+
 <div class="btn-group filter-button-group btn-no-shadow">
-    <form id="coins-import" class="form-horizontal" role="form" method="POST" action="" enctype="multipart/form-data">
-        {{ csrf_field() }}
-        <div class="row">
-            <div class="col-md-8">
-                <a href="javascript:void(0);" class="file btn-outline-primary grid-refresh btn-mini">选择文件
-                    <input type="file" name="file" id="coins-file">
-                </a>
+  <form id="coins-import" class="form-horizontal" role="form" method="POST" action="" enctype="multipart/form-data">
+    {{ csrf_field() }}
+    <!-- Button trigger modal -->
+    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#goodsImport">
+        导入数据
+    </button>
+  <!-- Modal -->
+    <div class="modal fade" id="goodsImport" tabindex="-1" role="dialog" aria-labelledby="title" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+            <h5 class="modal-title" id="modalLongTitle">导入数据</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
             </div>
-            <div class="col-md-8">
-                <button type="submit" class="btn btn-outline-primary grid-refresh btn-mini">
-                    <i class="fa fa-btn fa-sign-in"></i>导 入
-                </button>
+
+                <div class="modal-body">
+                    <div class="form-group">
+                        <a href="javascript:void(0);" class="file btn-outline-primary grid-refresh btn-mini">选择文件
+                            <input type="file" name="file" id="coins-file">
+                        </a>
+                    </div>
+                </div>
+            <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">取消</button>
+            <button type="submit" class="btn btn-primary">上传</button>
             </div>
         </div>
-    </form>
+        </div>
+    </div>
+</form>
 </div>
